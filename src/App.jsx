c2e06fc1dc48378148e3af89f06408dffc687478
Ksenia <baranova-ksenia1990@gmail.com>
2022-12-07 16:25:36 +0200

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import Home from './pages/home';
+import Offers from './pages/offers';
+import Profile from './pages/profile';
+import SignUp from './pages/signUp';
+import SignIn from './pages/signIn';
+import ResetPassword from './pages/resetPassword';
+
+import { Routes, Route } from 'react-router-dom';
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </>
+  );
 }
